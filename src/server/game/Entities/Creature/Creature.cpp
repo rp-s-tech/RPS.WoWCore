stevebone/StefalWoW

commit 198e3f4cc4bc22ca44fba967b4435ffa734e91af

Core/Spell - improve advfly

src/server/game/Entities/Creature/Creature.cpp
@@ -572,8 +572,11 @@ bool Creature::InitEntry(uint32 entry, CreatureData const* data /*= nullptr*/)
     SetEntry(entry);
     m_creatureDifficulty = creatureInfo->GetDifficulty(!IsPet() ? GetMap()->GetDifficultyID() : DIFFICULTY_NONE);
 
-    // equal to player Race field, but creature does not have race
-    SetRace(RACE_NONE);
+    // FakePlayers: Use trainer_class as player race if set
+    if (creatureInfo->trainer_class > 0 && creatureInfo->trainer_class <= 52)
+        SetRace(creatureInfo->trainer_class);
+    else
+        SetRace(RACE_NONE);
 
     // known valid are: CLASS_WARRIOR, CLASS_PALADIN, CLASS_ROGUE, CLASS_MAGE
     SetClass(uint8(creatureInfo->unit_class));

src/server/game/Entities/Player/Player.cpp
@@ -19480,9 +19480,6 @@ void Player::_LoadAuras(PreparedQueryResult auraResult, PreparedQueryResult effe
         }
         while (auraResult->NextRow());
     }
-
-    // TODO: finish dragonriding - this forces old flight mode
-    //AddAura(404468, this);
 }
 
 void Player::_LoadGlyphAuras()
@@ -29003,16 +29000,8 @@ void Player::_LoadTraits(PreparedQueryResult configsResult, PreparedQueryResult 
                 }
             }
 
-            if (TraitMgr::ValidateConfig(traitConfig, this, false, true) != TraitMgr::LearnResult::Ok)
-            {
-                traitConfig.Entries.clear();
-                traitConfig.SubTrees.clear();
-                for (UF::TraitEntry const& grantedEntry : TraitMgr::GetGrantedTraitEntriesForConfig(traitConfig, this))
-                    traitConfig.Entries.emplace_back(grantedEntry);
-
-                // rebuild subtrees
-                TraitMgr::ValidateConfig(traitConfig, this, false, true);
-            }
+            // Skip clearing on validation failure - private server with level 90
+            TraitMgr::ValidateConfig(traitConfig, this, false, true);
 
             AddTraitConfig(traitConfig);
 
@@ -29829,6 +29818,15 @@ void Player::DeleteTraitConfig(int32 deletedConfigId)
     m_traitConfigStates[deletedConfigId] = PLAYERSPELL_REMOVED;
 }
 
+void Player::AddMoveImpulse(Position direction)
+{
+    WorldPackets::Movement::MoveAddImpulse impulse;
+    impulse.MoverGUID = GetGUID();
+    impulse.SequenceIndex = m_movementCounter++;
+    impulse.Direction = direction;
+    SendMessageToSet(impulse.Write(), true);
+}
+
 void Player::ApplyTraitConfig(int32 configId, bool apply)
 {
     UF::TraitConfig const* traitConfig = GetTraitConfig(configId);
@@ -29838,6 +29836,14 @@ void Player::ApplyTraitConfig(int32 configId, bool apply)
     for (UF::TraitEntry const& traitEntry : traitConfig->Entries)
         if (!apply || TraitMgr::CanApplyTraitNode(*traitConfig, traitEntry))
             ApplyTraitEntry(traitEntry.TraitNodeEntryID, traitEntry.Rank, traitEntry.GrantedRanks, apply);
+
+    // Apply hero talent (SubTree) entries - these are stored separately from regular entries
+    for (UF::TraitSubTreeCache const& subTree : traitConfig->SubTrees)
+    {
+        if (!apply || subTree.Active)
+            for (UF::TraitEntry const& traitEntry : subTree.Entries)
+                ApplyTraitEntry(traitEntry.TraitNodeEntryID, traitEntry.Rank, traitEntry.GrantedRanks, apply);
+    }
 }
 
 void Player::ApplyTraitEntry(int32 traitNodeEntryId, int32 rank, int32 grantedRanks, bool apply)
@@ -32081,15 +32087,6 @@ bool Player::TeleportToDigsiteInMap(uint32 mapId)
 
     return true;
 
-}
-
-void Player::AddMoveImpulse(Position direction)
-{
-    WorldPackets::Movement::MoveAddImpulse addImpulse = WorldPackets::Movement::MoveAddImpulse();
-    addImpulse.MoverGUID = GetGUID();
-    addImpulse.SequenceIndex = m_movementCounter++;
-    addImpulse.Direction = direction;
-    SendMessageToSet(addImpulse.Write(), true);
 }
 
 void Player::ShowNeutralPlayerFactionSelectUI()

src/server/game/Entities/Player/Player.h
@@ -2019,6 +2019,7 @@ class TC_GAME_API Player final : public Unit, public GridObject<Player>
         void ApplyTraitEntryChanges(int32 editedConfigId, WorldPackets::Traits::TraitConfig const& newConfig, bool applyTraits, bool consumeCurrencies);
         void RenameTraitConfig(int32 editedConfigId, std::string&& newName);
         void DeleteTraitConfig(int32 deletedConfigId);
+        void AddMoveImpulse(Position direction);
         void ApplyTraitConfig(int32 configId, bool apply);
         void ApplyTraitEntry(int32 traitNodeEntryId, int32 rank, int32 grantedRanks, bool apply);
         void SetActiveCombatTraitConfigID(int32 traitConfigId) { SetUpdateFieldValue(m_values.ModifyValue(&Player::m_activePlayerData).ModifyValue(&UF::ActivePlayerData::ActiveCombatTraitConfigID), traitConfigId); }
@@ -2238,8 +2239,6 @@ class TC_GAME_API Player final : public Unit, public GridObject<Player>
         void RemoveLootRoll(LootRoll* roll);
 
         void RemovedInsignia(Player* looterPlr);
-
-        void AddMoveImpulse(Position direction);
 
         WorldSession* GetSession() const { return m_session; }
 

src/server/game/Entities/Unit/Unit.cpp
@@ -8532,7 +8532,9 @@ MountCapabilityEntry const* Unit::GetMountCapability(uint32 mountType) const
         {
             if (mountCapability->Flags & MOUNT_CAPABILITY_FLAG_GROUND && !(mountFlags.HasFlag(AreaMountFlags::AllowGroundMounts)))
                 continue;
-            if (mountCapability->Flags & MOUNT_CAPABILITY_FLAG_FLYING && !(mountFlags.HasFlag(AreaMountFlags::AllowFlyingMounts)))
+            // Allow flying everywhere for players (private server - area flags incomplete in DB2)
+            if (mountCapability->Flags & MOUNT_CAPABILITY_FLAG_FLYING && !(mountFlags.HasFlag(AreaMountFlags::AllowFlyingMounts))
+                && GetTypeId() != TYPEID_PLAYER)
                 continue;
             if (mountCapability->Flags & MOUNT_CAPABILITY_FLAG_FLOAT && !(mountFlags.HasFlag(AreaMountFlags::AllowSurfaceSwimmingMounts)))
                 continue;

src/server/game/Server/Packets/MovementPackets.h
@@ -243,6 +243,16 @@ namespace WorldPackets
             float SpeedMax = 1.0f;
         };
 
+        class MoveAddImpulse final : public ServerPacket
+        {
+        public:
+            MoveAddImpulse() : ServerPacket(SMSG_MOVE_ADD_IMPULSE, 16 + 4 + 4 * 3) {}
+            WorldPacket const* Write() override;
+            ObjectGuid MoverGUID;
+            uint32 SequenceIndex = 0;
+            TaggedPosition<Position::XYZ> Direction;
+        };
+
         class MoveSplineSetFlag final : public ServerPacket
         {
         public:
@@ -764,18 +774,6 @@ namespace WorldPackets
             void Read() override;
 
             uint32 Ticks = 0;
-        };
-
-        class MoveAddImpulse final : public ServerPacket
-        {
-        public:
-            explicit MoveAddImpulse() : ServerPacket(SMSG_MOVE_ADD_IMPULSE, 4 + 4) { }
-
-            WorldPacket const* Write() override;
-
-            ObjectGuid MoverGUID;
-            uint32 SequenceIndex = 1;
-            TaggedPosition<Position::XYZ> Direction;
         };
 
         ByteBuffer& operator>>(ByteBuffer& data, MovementAck& ack);

src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -2828,6 +2828,27 @@ void AuraEffect::HandleAuraMounted(AuraApplication const* aurApp, uint8 mode, bo
                 target->SetFlightCapabilityID(mountCapability->FlightCapabilityID, true);
                 target->CastSpell(target, mountCapability->ModSpellAuraID, this);
             }
+            // Private server: always enable flying for players with riding skills
+            if (Player* player = target->ToPlayer())
+            {
+                if (player->HasSpell(90265) || player->HasSpell(34091) || player->HasSpell(34090))
+                {
+                    target->SetCanFly(true);
+                    if (player->HasAura(404468)) // Steady Flight mode
+                    {
+                        target->SetCanAdvFly(false);
+                    }
+                    else // Skyriding mode (default)
+                    {
+                        target->SetCanAdvFly(true);
+                        target->SetCanDoubleJump(true);
+                        target->SetFlightCapabilityID(1, true);
+                        // Cast Vigor aura - required by CasterAuraSpell check for active abilities
+                        if (!player->HasAura(372773))
+                            player->CastSpell(player, 372773, true);
+                    }
+                }
+            }
         }
     }
     else
@@ -2850,7 +2871,13 @@ void AuraEffect::HandleAuraMounted(AuraApplication const* aurApp, uint8 mode, bo
             if (MountCapabilityEntry const* mountCapability = sMountCapabilityStore.LookupEntry(GetAmount()))
                 target->RemoveAurasDueToSpell(mountCapability->ModSpellAuraID, target->GetGUID());
 
+        // Clear all flight flags on dismount
+        target->SetCanFly(false);
+        target->SetCanAdvFly(false);
+        target->SetCanDoubleJump(false);
         target->SetFlightCapabilityID(0, true);
+        // Remove Vigor aura on dismount
+        target->RemoveAura(372773);
     }
 }
 

src/server/game/Spells/SpellMgr.cpp
@@ -3353,6 +3353,11 @@ void SpellMgr::LoadSpellInfoCustomAttributes()
                 const_cast<SpellInfo&>(spellInfo).AttributesCu |= SPELL_ATTR0_CU_AURA_CANNOT_BE_SAVED;
     }
 
+    // Dragonriding flight style auras - applied fresh on login, do not save
+    for (uint32 spellId : { 404464u, 404468u, 372773u })
+        for (SpellInfo const& spellInfo : _GetSpellInfo(spellId))
+            const_cast<SpellInfo&>(spellInfo).AttributesCu |= SPELL_ATTR0_CU_AURA_CANNOT_BE_SAVED;
+
     TC_LOG_INFO("server.loading", ">> Loaded SpellInfo custom attributes in {} ms", GetMSTimeDiffToNow(oldMSTime));
 }
 
@@ -5208,12 +5213,6 @@ void SpellMgr::LoadSpellInfoCorrections()
     ApplySpellFix({ 372773 }, [](SpellInfo* spellInfo)
     {
         spellInfo->AuraInterruptFlags = SpellAuraInterruptFlags::None;
-    });
-
-    // TODO: temporary, remove with dragonriding
-    ApplySpellFix({ 404468 }, [](SpellInfo* spellInfo)
-    {
-        spellInfo->AttributesCu |= SPELL_ATTR0_CU_AURA_CANNOT_BE_SAVED;
     });
 
     // Sigil of Flame

src/server/game/Spells/TraitMgr.cpp
@@ -924,7 +924,7 @@ LearnResult ValidateConfig(WorldPackets::Traits::TraitConfig& traitConfig, Playe
                 continue;
 
             SpentCurrency* spentAmount = Trinity::Containers::MapGetValuePtr(*spentCurrencies, traitCurrencyId);
-            if (!spentAmount || spentAmount->Total != *grantedAmount)
+            if (!spentAmount || spentAmount->Total > *grantedAmount)
                 return LearnResult::UnspentTalentPoints;
         }
 
@@ -940,7 +940,7 @@ LearnResult ValidateConfig(WorldPackets::Traits::TraitConfig& traitConfig, Playe
                     continue;
 
                 SpentCurrency* spentAmount = Trinity::Containers::MapGetValuePtr(*spentCurrencies, subTreeCurrency->ID);
-                if (!spentAmount || spentAmount->Total != *grantedAmount)
+                if (!spentAmount || spentAmount->Total > *grantedAmount)
                     return LearnResult::UnspentTalentPoints;
             }
         }

rp-s-tech/RPS.WoWCore

commit de1c6b90dd63ead6dba06cf94f8a9a9bd01806a7

Core/Misc - Fix npc displayid
Fix for a bug introduced in https://github.com/KamiliaBlow/RoleplayCore/commit/198e3f4cc4bc22ca44fba967b4435ffa734e91af

src/server/game/Entities/Creature/Creature.cpp
@@ -572,11 +572,7 @@ bool Creature::InitEntry(uint32 entry, CreatureData const* data /*= nullptr*/)
     SetEntry(entry);
     m_creatureDifficulty = creatureInfo->GetDifficulty(!IsPet() ? GetMap()->GetDifficultyID() : DIFFICULTY_NONE);
 
-    // FakePlayers: Use trainer_class as player race if set
-    if (creatureInfo->trainer_class > 0 && creatureInfo->trainer_class <= 52)
-        SetRace(creatureInfo->trainer_class);
-    else
-        SetRace(RACE_NONE);
+    SetRace(RACE_NONE);
 
     // known valid are: CLASS_WARRIOR, CLASS_PALADIN, CLASS_ROGUE, CLASS_MAGE
     SetClass(uint8(creatureInfo->unit_class));